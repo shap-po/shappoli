451e84dab595910266a0c498aed13245ee070a29
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-08-25 02:08:50 +0300

    refactor: Move TrinketSlotData serialize functions

--- src/main/java/com/github/shap_po/shappoli/data/TrinketSlotData.java
+++ src/main/java/com/github/shap_po/shappoli/data/TrinketSlotData.java
@@ -2,9 +2,16 @@ package com.github.shap_po.shappoli.data;
 
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.SlotType;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
 import org.jetbrains.annotations.Nullable;
 
 public class TrinketSlotData {
+    public static final SerializableData DATA = new SerializableData()
+        .add("name", SerializableDataTypes.STRING, null)
+        .add("group", SerializableDataTypes.STRING, null)
+        .add("index", SerializableDataTypes.INT, null);
+
     public final @Nullable String name;
     public final @Nullable String group;
     public final @Nullable Integer index;
@@ -20,5 +27,22 @@ public class TrinketSlotData {
         return (group == null || group.equals(slotType.getGroup())) &&
             (name == null || name.equals(slotType.getName())) &&
             (index == null || index == slotReference.index());
+    }
+
+
+    public static TrinketSlotData fromData(SerializableData.Instance dataInstance) {
+        return new TrinketSlotData(
+            dataInstance.get("name"),
+            dataInstance.get("group"),
+            dataInstance.isPresent("index") ? dataInstance.getInt("index") : null
+        );
+    }
+
+    public static SerializableData.Instance toData(SerializableData data, TrinketSlotData trinketSlotData) {
+        SerializableData.Instance dataInstance = data.new Instance();
+        dataInstance.set("name", trinketSlotData.name);
+        dataInstance.set("group", trinketSlotData.group);
+        dataInstance.set("index", trinketSlotData.index);
+        return dataInstance;
     }
 }

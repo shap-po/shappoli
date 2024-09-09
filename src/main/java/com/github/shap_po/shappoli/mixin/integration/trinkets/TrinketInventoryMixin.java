dd118ff8c1f24af8ad7ca4f6aa134f25f8c8277b
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-09-09 23:05:37 +0300

    feat: Add TrinketInventory lower size bound

--- src/main/java/com/github/shap_po/shappoli/mixin/integration/trinkets/TrinketInventoryMixin.java
+++ src/main/java/com/github/shap_po/shappoli/mixin/integration/trinkets/TrinketInventoryMixin.java
@@ -2,9 +2,12 @@ package com.github.shap_po.shappoli.mixin.integration.trinkets;
 
 import com.github.shap_po.shappoli.util.InventoryUtil;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.emi.trinkets.api.TrinketInventory;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -15,5 +18,14 @@ public class TrinketInventoryMixin {
         LivingEntity entity = ((TrinketInventory) (Object) this).getComponent().getEntity();
         InventoryUtil.setHolder(stack, entity);
         return stack;
+    }
+
+    @WrapOperation(
+        method = "update",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;ofSize(ILjava/lang/Object;)Lnet/minecraft/util/collection/DefaultedList;")
+    )
+    private <E> DefaultedList<E> shappoli$fixNegativeSize(int size, E defaultValue, Operation<DefaultedList<E>> original) {
+        size = Math.max(size, 0);
+        return (original).call(size, defaultValue);
     }
 }

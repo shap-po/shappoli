9c913254b6c8559b03a73054b6599ae4f697b9b5
shap-po <slavko.ukrop@gmail.com>
2024-09-23 11:53:01 +0300

    feat: Add shappoli command

--- src/main/java/com/github/shap_po/shappoli/Shappoli.java
+++ src/main/java/com/github/shap_po/shappoli/Shappoli.java
@@ -1,8 +1,10 @@
 package com.github.shap_po.shappoli;
 
+import com.github.shap_po.shappoli.command.ShappoliCommand;
 import com.github.shap_po.shappoli.integration.ModIntegrations;
 import com.github.shap_po.shappoli.registry.ModPowers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +17,10 @@ public class Shappoli implements ModInitializer {
     public void onInitialize() {
         ModPowers.register();
         ModIntegrations.register();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            ShappoliCommand.register(dispatcher);
+        });
     }
 
     public static Identifier identifier(String path) {

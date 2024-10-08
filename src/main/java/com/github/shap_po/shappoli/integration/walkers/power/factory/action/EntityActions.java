2f36881d1b7c09e654a2fab0419ef7b6ffa996bc
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-10-08 23:17:28 +0300

    feat: Add UseShapeAbilityAction

--- src/main/java/com/github/shap_po/shappoli/integration/walkers/power/factory/action/EntityActions.java
+++ src/main/java/com/github/shap_po/shappoli/integration/walkers/power/factory/action/EntityActions.java
@@ -3,6 +3,7 @@ package com.github.shap_po.shappoli.integration.walkers.power.factory.action;
 import com.github.shap_po.shappoli.integration.walkers.power.factory.action.entity.ChangeShapeAbilityCooldownAction;
 import com.github.shap_po.shappoli.integration.walkers.power.factory.action.entity.ShapeActionAction;
 import com.github.shap_po.shappoli.integration.walkers.power.factory.action.entity.SwitchShapeAction;
+import com.github.shap_po.shappoli.integration.walkers.power.factory.action.entity.UseShapeAbilityAction;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.apoli.registry.ApoliRegistries;
 import net.minecraft.entity.Entity;
@@ -13,6 +14,7 @@ public class EntityActions {
         register(ChangeShapeAbilityCooldownAction.getFactory());
         register(ShapeActionAction.getFactory());
         register(SwitchShapeAction.getFactory());
+        register(UseShapeAbilityAction.getFactory());
     }
 
     private static void register(ActionFactory<Entity> actionFactory) {

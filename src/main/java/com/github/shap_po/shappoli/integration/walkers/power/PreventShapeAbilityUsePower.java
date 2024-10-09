13e9d40fcf64e0d6a87aadceb1d216e5e512424b
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-10-09 16:59:31 +0300

    feat: Improve PreventShapeAbilityUsePower & HasShapeAbilityCondition

--- src/main/java/com/github/shap_po/shappoli/integration/walkers/power/PreventShapeAbilityUsePower.java
+++ src/main/java/com/github/shap_po/shappoli/integration/walkers/power/PreventShapeAbilityUsePower.java
@@ -1,36 +1,58 @@
 package com.github.shap_po.shappoli.integration.walkers.power;
 
 import com.github.shap_po.shappoli.Shappoli;
+import com.github.shap_po.shappoli.integration.walkers.registry.ShappoliWalkersRegistries;
 import com.github.shap_po.shappoli.integration.walkers.util.WalkersUtil;
+import com.github.shap_po.shappoli.util.MiscUtil;
 import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
+import tocraft.walkers.ability.AbilityRegistry;
+import tocraft.walkers.ability.ShapeAbility;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class PreventShapeAbilityUsePower extends Power {
     private final Predicate<Pair<Entity, Entity>> bientityCondition;
+    private final List<? extends Class<? extends ShapeAbility<?>>> abilities;
 
     public PreventShapeAbilityUsePower(
         PowerType<?> type,
         LivingEntity entity,
-        Predicate<Pair<Entity, Entity>> bientityCondition
+        Predicate<Pair<Entity, Entity>> bientityCondition,
+        List<? extends Class<? extends ShapeAbility<?>>> abilities
     ) {
         super(type, entity);
         this.bientityCondition = bientityCondition;
+        this.abilities = abilities;
     }
 
     public boolean doesApply() {
         if (!(entity instanceof PlayerEntity player)) {
             return false;
         }
-        return bientityCondition == null || bientityCondition.test(new Pair<>(entity, WalkersUtil.getShape(player)));
+
+        LivingEntity shape = WalkersUtil.getShape(player);
+        if (bientityCondition != null && bientityCondition.test(new Pair<>(entity, shape))) {
+            return true;
+        }
+
+        ShapeAbility<?> shapeAbility = AbilityRegistry.get(shape);
+        if (shapeAbility == null) {
+            return false;
+        }
+
+        return abilities.stream().anyMatch(a -> a.isInstance(shapeAbility));
     }
 
     public static PowerFactory createFactory() {
@@ -38,9 +60,16 @@ public class PreventShapeAbilityUsePower extends Power {
             Shappoli.identifier("prevent_shape_ability_use"),
             new SerializableData()
                 .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null)
+                .add("ability", SerializableDataTypes.IDENTIFIER, null)
+                .add("abilities", SerializableDataTypes.IDENTIFIERS, null)
             ,
-            data -> (type1, player) -> new PreventShapeAbilityUsePower(type1, player,
-                data.get("bientity_condition")
+            data -> (type, player) -> new PreventShapeAbilityUsePower(type, player,
+                data.get("bientity_condition"),
+                MiscUtil.<Identifier>listFromData(data, "ability", "abilities")
+                    .stream()
+                    .map(ShappoliWalkersRegistries.SHAPE_ABILITY_TYPE::get)
+                    .filter(Objects::nonNull)
+                    .toList()
             )
         ).allowCondition();
     }
--- src/main/java/com/github/shap_po/shappoli/integration/walkers/power/factory/condition/entity/HasShapeAbilityCondition.java
+++ src/main/java/com/github/shap_po/shappoli/integration/walkers/power/factory/condition/entity/HasShapeAbilityCondition.java
@@ -3,6 +3,7 @@ package com.github.shap_po.shappoli.integration.walkers.power.factory.condition.
 import com.github.shap_po.shappoli.Shappoli;
 import com.github.shap_po.shappoli.integration.walkers.registry.ShappoliWalkersRegistries;
 import com.github.shap_po.shappoli.integration.walkers.util.WalkersUtil;
+import com.github.shap_po.shappoli.util.MiscUtil;
 import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
@@ -12,6 +13,9 @@ import net.minecraft.util.Identifier;
 import tocraft.walkers.ability.AbilityRegistry;
 import tocraft.walkers.ability.ShapeAbility;
 
+import java.util.List;
+import java.util.Objects;
+
 public class HasShapeAbilityCondition {
     public static boolean condition(SerializableData.Instance data, Entity entity) {
         if (!(entity instanceof LivingEntity livingEntity)) {
@@ -20,8 +24,8 @@ public class HasShapeAbilityCondition {
 
         LivingEntity shape = WalkersUtil.getEffectiveShape(livingEntity);
 
-        Identifier ability = data.getId("ability");
-        if (ability == null) {
+        List<Identifier> abilities = MiscUtil.listFromData(data, "ability", "abilities");
+        if (abilities.isEmpty()) {
             return AbilityRegistry.has(shape);
         }
 
@@ -30,8 +34,11 @@ public class HasShapeAbilityCondition {
             return false;
         }
 
-        Class<? extends ShapeAbility<?>> shapeAbilityClass = ShappoliWalkersRegistries.SHAPE_ABILITY_TYPE.get(ability);
-        return shapeAbilityClass != null && shapeAbilityClass.isInstance(shapeAbility);
+        return abilities
+            .stream()
+            .map(ShappoliWalkersRegistries.SHAPE_ABILITY_TYPE::get)
+            .filter(Objects::nonNull)
+            .anyMatch(a -> a.isInstance(shapeAbility));
     }
 
 
@@ -40,6 +47,7 @@ public class HasShapeAbilityCondition {
             Shappoli.identifier("has_shape_ability"),
             new SerializableData()
                 .add("ability", SerializableDataTypes.IDENTIFIER, null)
+                .add("abilities", SerializableDataTypes.IDENTIFIERS, null)
             ,
             HasShapeAbilityCondition::condition
         );
